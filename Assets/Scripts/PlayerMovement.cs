d4d0da6dcbf25eefcce65297dd65aed2ff046191
kevinsbarski <kevinsbarski@gmail.com>
2023-08-18 17:20:18 +0300

    code changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-     public float moveSpeed = 1.5f;
-     public float rotationSpeed = 5.0f;
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private float rotationSpeed = 5.0f;
      private Rigidbody2D rb;
-
+     private float delatX;
+     private float deltaY;
+     private Vector2 movement;
+     
      public float minX, maxX, minY, maxY; // Define your bounds here
 
     // Start is called before the first frame update
@@ -20,18 +24,25 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         //move with key pressed
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized * moveSpeed;
+       
         //rb.velocity = movement;
 
         //bounds
-         Vector3 newPos = transform.position + new Vector3(movement.x, movement.y, 0) * Time.deltaTime;
-         newPos.x = Mathf.Clamp(newPos.x, Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x,
-                               Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x);
-         newPos.y = Mathf.Clamp(newPos.y, Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y,
-                               Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y);
-         transform.position = newPos;
+        bounds();
+        // if (transform.position.y >= 2)
+        // {
+        //     transform.position = new Vector3(transform.position.x, 4.5f, 0);
+        // }
+        // else if(transform.position.y <= -4.3f)
+        // {
+        //     transform.position = new Vector3(transform.position.x, -4.5f, 0);
+        // }
+         // Vector3 newPos = transform.position + new Vector3(movement.x, movement.y, 0) * Time.deltaTime;
+         // newPos.x = Mathf.Clamp(newPos.x, Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x,
+         //                       Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x);
+         // newPos.y = Mathf.Clamp(newPos.y, Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y,
+         //                       Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y);
+         // transform.position = newPos;
 
         
         //rotation with mouse
@@ -41,5 +52,33 @@ public class PlayerMovement : MonoBehaviour
         rb.rotation = angle-90;
 
     }
-    
+
+    private void FixedUpdate()
+    {
+        delatX = Input.GetAxis("Horizontal"); 
+        deltaY = Input.GetAxis("Vertical");
+        movement = new Vector2(delatX, deltaY).normalized * moveSpeed;
+        rb.velocity = movement;
+    }
+
+    private void bounds()
+    {
+        if (transform.position.y >= 4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.5f, 0);
+        }
+        else if(transform.position.y <= -4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.5f, 0);
+        }
+        
+        if (transform.position.x >= 9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
+        else if(transform.position.x <= -9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+    }
 }
